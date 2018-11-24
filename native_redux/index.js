34ec63c90497b8d9f9a2398d50d5381fd98b6cb2
GaponVasa <gapon.vasa@ukr.net>
2018-11-24 23:35:13 +0200

    first commit

---
+++ native_redux/index.js
@@ -0,0 +1,49 @@
+'use strict';
+
+//Reducer
+function updateState(state, action){
+  if(action.type === 'INCREMENT'){
+    return {count: state.count + action.amount};
+  }else if(action.type === 'DECREMENT'){
+    return {count: state.count - action.amount};
+  }else{
+    return state;
+  }
+}
+//store
+class Store{
+  constructor(updateState, state){
+    this._updateState = updateState;
+    this._state = state;
+    this._callbacks = [];
+  }
+  get state(){
+    return this._state;
+  }
+
+  update(action){
+    this._state = this._updateState(this._state, action);
+    this._callbacks.forEach(callback => callback());
+  }
+
+  subscribe(callback){
+    this._callbacks.push(callback);
+    return () => this._callbacks = this._callbacks.filter(cd => cd !== callback);
+  }
+}
+
+const initalState = {count: 0};
+
+const store = new Store(updateState, initalState);
+
+//action
+const incrementAction = {type: 'INCREMENT', amount: 5};
+const decrementAction = {type: 'DECREMENT', amount: 3};
+//
+const unsubscribe = store.subscribe(()=>console.log('State.changed 1', store.state));
+store.subscribe(()=>console.log('State.changed 2', store.state));
+
+store.update(incrementAction);
+unsubscribe();
+store.update(decrementAction);
+store.update({});
