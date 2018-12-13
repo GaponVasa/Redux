40e1a4eb963991b0b337c3e4056c06139c48670c
GaponVasa <gapon.vasa@ukr.net>
2018-12-13 16:56:07 +0200

    forgot to add file redux.js

---
+++ native_redux/redux.js
@@ -0,0 +1,18 @@
+function createStore(reducer, initialState) {
+  let state = initialState;
+  let callbacks = [];
+
+  const getState = () => state;
+
+  const dispatch = action => {
+    state = reducer(state, action);
+    callbacks.forEach(callback => callback());
+  }
+
+  const subscribe = callback => {
+    callbacks.push(callback);
+    return () => callbacks.forEach(el => el !== callback);
+  }
+
+  return { getState, dispatch, subscribe };
+}
