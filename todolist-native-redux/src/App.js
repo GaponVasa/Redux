b63d0a7c67967288e227a7fe95a369a11b4c8277
GaponVasa <gapon.vasa@ukr.net>
2018-12-30 13:24:32 +0200

    fixed some known issues and decomposed unnecessarily on individual files

--- todolist-native-redux/src/App.js
+++ todolist-native-redux/src/App.js
@@ -1,50 +1,12 @@
 import React, { Component } from 'react';
-import ShowCard from './components/show_card.js';
 
-import createStore from './redux.js';
+import store from './store';
+import addAction from './actions/action-add';
+import editAction from './actions/action-edit';
+import deleteAction from './actions/action-delete';
 import Button from './components/button.js';
 import Input from './components/input.js';
-
-const initalState = { cards: [{ id: 0, text: 'Learn React' }] };
-
-const store = createStore(reducer, initalState);
-
-function reducer(state, action) {
-  switch (action.type) {
-    case "ADD":
-      return { cards: [...state.cards, action.card] };
-    case "EDIT":
-      return {
-        cards: state.cards.map((el) => {
-          if (el.id === action.card.id) {
-            el.text = action.card.text
-          }
-          return el;
-        })
-      };
-    case "DELETE":
-      return { 
-        cards: state.cards.filter((el) => { 
-          if (el.id !== action.id) { return el } 
-        }) 
-      };
-    default:
-      return state;
-  }
-}
-
-//action
-const addAction = card => {
-  return { type: "ADD", card };
-};
-
-const editAction = card => {
-  return { type: "EDIT", card };
-};
-
-const deleteAction = id => {
-  return { type: "DELETE", id };
-};
+import ShowCard from './containers/show_card';
 
 class App extends Component {
   state = {
@@ -75,22 +37,19 @@ class App extends Component {
     const addText = this.state.value;
     const cards = store.getState().cards;
     const length = cards.length;
-    const addId = cards[length - 1].id + 1;
+    const addId = length===0 ? 0 : cards[length - 1].id + 1;
     const card = { id: addId, text: addText };
 
     store.dispatch(addAction(card));
-    console.log(store.getState().cards);
     this.setState({ value: '' });
   };
 
   editCard = (card) => {
     store.dispatch(editAction(card));
-    console.log(store.getState().cards);
   };
 
   deleteCard = (id) => {
     store.dispatch(deleteAction(id));
-    console.log(store.getState().cards);
   }
 
   handleChange = (event) => {
