f5aa65d29a7f4801331d0c2ebfc41754468b9e43
GaponVasa <gapon.vasa@ukr.net>
2019-01-04 19:35:33 +0200

    add functionality for ADD and DELETE

---
+++ task-manager/src/store/initial-state.js
@@ -0,0 +1,35 @@
+export const initialState = {
+  tasks : [
+    {
+      id: 0,
+      bodyTask: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+      changed:false
+    },
+    {
+      id: 1,
+      bodyTask: "Lorem ipsum dolor sit amet.",
+      changed:false
+    },
+    {
+      id: 2,
+      bodyTask: "Lorem ipsum dolor.",
+      changed:false
+    },
+    {
+      id: 3,
+      bodyTask: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, voluptates.",
+      changed:false
+    },
+    {
+      id: 4,
+      bodyTask: "Lorem ipsum dolor sit amet, consectetur adipisicing.",
+      changed:false
+    },
+    {
+      id: 5,
+      bodyTask: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam vitae accusantium sint magnam corporis, omnis nulla eum expedita quidem animi laudantium, et eveniet. Aut delectus, saepe incidunt veritatis fuga eligendi.",
+      changed:false
+    },
+  ],
+  filtered: false,
+}
