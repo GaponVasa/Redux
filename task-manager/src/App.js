fcc182769edd45ddcf3935f7b0a411fbd248cb2a
GaponVasa <gapon.vasa@ukr.net>
2019-01-07 15:03:08 +0200

    changed the location of the files

--- task-manager/src/App.js
+++ task-manager/src/App.js
@@ -4,9 +4,13 @@ import PropTypes from "prop-types";
 import InputGroup from "./components/input-group/input-group";
 import FormCheckInline from "./components/form-check-inline/form-check-inline";
 import "./App.css";
-import Header from "./containers/header";
-import TaskList from "./containers/task-list";
-import { actionAddTask, actionDeleteTask, actionEditTask, actionCompleteTask } from "./store/store";
+import TaskList from "./components/task-list/task-list";
+import {
+  actionAddTask,
+  actionDeleteTask,
+  actionEditTask,
+  actionCompleteTask
+} from "./actions/actionCreator";
 
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
@@ -15,8 +19,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      addValue: "",
-      searchValue: ""
+      addValue: ""
     };
   }
 
@@ -46,38 +49,30 @@ class App extends Component {
     const { actionCompleteTask } = this.props;
     actionCompleteTask(currentId);
   };
-  
+
   onclickCompleteAllButton = () => {
     const { actionCompleteTask, tasks } = this.props;
-    tasks.forEach(el =>{
+    tasks.forEach(el => {
       actionCompleteTask(el.id);
     });
   };
 
   onclickEditButton = (currentId, currentValue) => {
-    const { actionEditTask } = this.props;    
+    const { actionEditTask } = this.props;
     actionEditTask(currentId, currentValue);
   };
 
-  onclick = () => {
-    console.log("click");
-  };
-
   render() {
-    console.log("App this.props", this.props);
     const { tasks } = this.props;
-    const { addValue, searchValue } = this.state;
+    const { addValue } = this.state;
     return (
       <div className="container d-flex flex-column border my-3">
-        <Header
-          classNameInputGroup={"input-group mr-0"}
-          classNameButton={"btn btn-outline-secondary"}
-          idButton={"button-addon"}
-          innerText={"Search"}
-          onclickButton={this.onclick}
-          values={searchValue}
-          onChange={this.onchangeSearchValue}
-        />
+        <div className="row mt-3 mx-3">
+          <div className="col-6 pl-0">
+            <h3>Task planning</h3>
+          </div>
+          <div className="col-6 pr-0" />
+        </div>
         <div className="row m-3">
           <InputGroup
             classNameInputGroup={"input-group col-12 px-0"}
@@ -113,8 +108,7 @@ App.propTypes = {
 
 const putStateToProps = state => {
   return {
-    tasks: state.tasks,
-    filtered: state.filtered
+    tasks: state.tasks
   };
 };
 
--- task-manager/src/components/form-check-inline/form-check-inline.jsx
+++ task-manager/src/components/form-check-inline/form-check-inline.jsx
@@ -1,56 +1,56 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Input from '../input/input';
-import randomId from '../../others/random_id.js';
-import './form-check-inline.css';
+import React from "react";
+import PropTypes from "prop-types";
+import Input from "../input/input";
+import randomId from "../../others/random_id.js";
+import "./form-check-inline.css";
 
 const FormCheckInline = props => {
   const {
-    classNameInline, 
-    textTask, 
-    onclick, 
-    flagInput, 
+    classNameInline,
+    textTask,
+    onclick,
+    flagInput,
     onchange,
-    editvalue, 
+    editvalue,
     complete
   } = props;
   const ID = "inlineCheckbox" + randomId();
   const addClass = "form-check-inline ";
-  const classInline = classNameInline =>{
+  const classInline = classNameInline => {
     return classNameInline ? addClass + classNameInline : addClass;
-  }
-  const returnLable = ()=>{
+  };
+  const returnLable = () => {
     const classCheckLable = "form-check-label";
-    const strikeClass = ' strikethrough';
+    const strikeClass = " strikethrough";
     return (
-      <label 
-        className={complete ? classCheckLable + strikeClass: classCheckLable}
+      <label
+        className={complete ? classCheckLable + strikeClass : classCheckLable}
         htmlFor={ID}
       >
         {textTask}
       </label>
-    )
-  }
-  const returnInput = ()=>{
+    );
+  };
+  const returnInput = () => {
     return (
-      <Input 
-        className="col" 
-        type="text" 
-        placeholder={'Enter text'}
+      <Input
+        className="col"
+        type="text"
+        placeholder={"Enter text"}
         value={editvalue}
         onChange={onchange}
       />
-    )
-  }
+    );
+  };
   return (
     <div className={classInline(classNameInline)}>
-      <Input 
-        className="form-check-input" 
-        type="checkbox" 
+      <Input
+        className="form-check-input"
+        type="checkbox"
         id={ID}
         onChange={onclick}
         value={true}
-        checked={complete ? 'checked' : null}
+        checked={complete ? "checked" : null}
       />
       {flagInput ? returnInput() : returnLable()}
     </div>
@@ -58,13 +58,13 @@ const FormCheckInline = props => {
 };
 
 FormCheckInline.propTypes = {
-  classNameInline: PropTypes.string, 
+  classNameInline: PropTypes.string,
   textTask: PropTypes.string,
-  flagInput:  PropTypes.bool,
+  flagInput: PropTypes.bool,
   onclick: PropTypes.func,
   onchange: PropTypes.func,
   editvalue: PropTypes.string,
-  complete:  PropTypes.bool,
+  complete: PropTypes.bool
 };
 
 export default FormCheckInline;
